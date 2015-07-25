70d814269101311179f71818f28e236b64da7d92
L.bdcdb <ludo.bouan@gmail.com>
2015-07-25 16:22:13 +0200

    play music

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load("music/GoodMorningFreedom.mp3")
 pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+echo("playing")
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
